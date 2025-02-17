b55acd11c01a47f42396af60c5172e9cbd67a91a
paulmachholz <paul.machholz@stud-mail.uni-wuerzburg.de>
2025-02-17 12:00:21 +0100

    FrequentWords added

--- functions.py
+++ functions.py
@@ -6,3 +6,15 @@ def PatternCount(a, b):
     return count
         
 
+def FrequentWords(text, k):
+    FrequentPatterns = set() #stores unique freq. k-mers
+    count = []  #array
+    for i in range(len(text) - k+1):
+        Pattern = text[i:i+k]   #extract k-mers
+        count.append(PatternCount(text, Pattern)) #stores k-mer in arrray
+    maxCount = max(count) #finds maximum count
+    for i in range(len(text) - k + 1): 
+        if count[i] == maxCount:     #identifying highest count
+            FrequentPatterns.add(text[i:i+k]) #add k-mer to set
+
+    return list(FrequentPatterns)
